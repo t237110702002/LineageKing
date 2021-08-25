23b209e8ecdbff11374fdba6ab339296ad131a3e
tyen <tyen@drfirst.com>
2021-08-25 10:12:48 +0800

    clean

--- src/main/java/com/tinatest/line_bot/controller/LineBotController.java
+++ src/main/java/com/tinatest/line_bot/controller/LineBotController.java
@@ -1,17 +1,9 @@
 package com.tinatest.line_bot.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.beans.EventHandler;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @Slf4j
@@ -19,12 +11,9 @@ public class LineBotController {
 
 
     @GetMapping("/hello")
-    @ResponseBody
     public ResponseEntity printHello() {
         System.out.println("hello");
         return ResponseEntity.ok("hello welcome to my app :");
-
-
     }
 
 
--- src/test/java/com/tinatest/line_bot/LineBotApplicationTests.java
+++ src/test/java/com/tinatest/line_bot/LineBotApplicationTests.java
@@ -1,13 +1,8 @@
 package com.tinatest.line_bot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LineBotApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
