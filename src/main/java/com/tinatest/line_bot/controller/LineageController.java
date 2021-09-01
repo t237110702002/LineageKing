bf0d08cf094ef44c96d317f60b6b4410fbb41031
tyen <tyen@drfirst.com>
2021-09-01 15:45:52 +0800

    0901 fix bug

--- src/main/java/com/tinatest/line_bot/controller/LineageController.java
+++ src/main/java/com/tinatest/line_bot/controller/LineageController.java
@@ -96,7 +96,7 @@ public class LineageController {
         if (!token.equals(adminToken)) {
             return "AUTH FAIL";
         }
-        Message msg = lineBotService.getMsg(command, "Ud62a356eedbea86f5231532bae38da4c");
+        Message msg = lineBotService.getMsg(command, "Ud62a356eedbea86f5231532bae38da4c", "Ud62a356eedbea86f5231532bae38da4c");
         client.pushMessage(new PushMessage("Ud62a356eedbea86f5231532bae38da4c", msg));
         return msg.toString();
     }
